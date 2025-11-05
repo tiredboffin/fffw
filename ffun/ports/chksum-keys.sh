ddc47c39111503775740cdb07397931c742d8b0c
tiredboffin <tiredboffin@gmail.com>
2025-11-05 21:11:29 +0300

    update key chksum script

--- ffun/ports/chksum-keys.sh
+++ ffun/ports/chksum-keys.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-find . -type f -name pubkey.der -exec md5sum {} +
+#find . -type f -name pubkey.der -exec md5sum {} +
+if [ "$(find . -type f -name "pubkey.der" -exec md5sum {} + | awk '{print $1}' | sort | uniq -d | wc -l)" -gt 0 ]; then
+  echo "matches found?!"
+  exit 0
+else
+  echo "OK"
+  exit 1
+fi
